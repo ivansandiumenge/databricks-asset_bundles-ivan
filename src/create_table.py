47052173d1a8f52d3952c375346cbf1ab9642ac4
ivan.sandiumenge@lovelytics.com <ivan.sandiumenge@lovelytics.com>
2025-12-05 19:13:32 +0000

    Minor fixes

--- src/create_table.py
+++ src/create_table.py
@@ -13,7 +13,7 @@ df = (
     spark.read.format("csv")
     .option("header", "true")
     .option("inferSchema", "true")
-    .load(f"/Volumes/{catalog}/{schema}/inputs/")
+    .load(f"/Volumes/{catalog}/{schema}/practice_volume/")
 )
 
 display(df)
